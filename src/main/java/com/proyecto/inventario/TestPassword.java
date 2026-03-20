105b6ba49679dd2e0fb6a8656c69af49f582b247
andre-exe <andre.gitbh@gmail.com>
2026-03-20 11:40:51 -0600

    jwt funcional

--- src/main/java/com/proyecto/inventario/TestPassword.java
+++ src/main/java/com/proyecto/inventario/TestPassword.java
@@ -4,7 +4,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class TestPassword {
     public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    System.out.println(encoder.encode("123456"));
+        
     }
 }
